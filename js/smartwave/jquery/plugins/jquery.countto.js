455496050db02e367c216513e88687b9ee07ad7f
Pedro Laris <peyi.god@gmail.com>
2018-07-06 16:32:16 -0700

    Theme instalation

---
+++ js/smartwave/jquery/plugins/jquery.countto.js
@@ -0,0 +1,191 @@
+
+/*
+Plugin Name: 	Count To
+Written by: 	Matt Huggins - https://github.com/mhuggins/jquery-countTo
+*/
+
+(function ($) {
+	$.fn.countTo = function (options) {
+		options = options || {};
+
+		return $(this).each(function () {
+			// set options for current element
+			var settings = $.extend({}, $.fn.countTo.defaults, {
+				from:            $(this).data('from'),
+				to:              $(this).data('to'),
+				speed:           $(this).data('speed'),
+				refreshInterval: $(this).data('refresh-interval'),
+				decimals:        $(this).data('decimals')
+			}, options);
+
+			// how many times to update the value, and how much to increment the value on each update
+			var loops = Math.ceil(settings.speed / settings.refreshInterval),
+				increment = (settings.to - settings.from) / loops;
+
+			// references & variables that will change with each update
+			var self = this,
+				$self = $(this),
+				loopCount = 0,
+				value = settings.from,
+				data = $self.data('countTo') || {};
+
+			$self.data('countTo', data);
+
+			// if an existing interval can be found, clear it first
+			if (data.interval) {
+				clearInterval(data.interval);
+			}
+			data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+			// initialize the element with the starting value
+			render(value);
+
+			function updateTimer() {
+				value += increment;
+				loopCount++;
+
+				render(value);
+
+				if (typeof(settings.onUpdate) == 'function') {
+					settings.onUpdate.call(self, value);
+				}
+
+				if (loopCount >= loops) {
+					// remove the interval
+					$self.removeData('countTo');
+					clearInterval(data.interval);
+					value = settings.to;
+
+					if (typeof(settings.onComplete) == 'function') {
+						settings.onComplete.call(self, value);
+					}
+				}
+			}
+
+			function render(value) {
+				var formattedValue = settings.formatter.call(self, value, settings);
+				$self.html(formattedValue);
+			}
+		});
+	};
+
+	$.fn.countTo.defaults = {
+		from: 0,               // the number the element should start at
+		to: 0,                 // the number the element should end at
+		speed: 1000,           // how long it should take to count between the target numbers
+		refreshInterval: 100,  // how often the element should be updated
+		decimals: 0,           // the number of decimal places to show
+		formatter: formatter,  // handler for formatting the value before rendering
+		onUpdate: null,        // callback method for every time the element is updated
+		onComplete: null       // callback method for when the element finishes updating
+	};
+
+	function formatter(value, settings) {
+		return value.toFixed(settings.decimals);
+	}
+}(jQuery));
+
+// Counter
+(function(theme, $) {
+
+	theme = theme || {};
+
+	var instanceName = '__counter';
+
+	var PluginCounter = function($el, opts) {
+		return this.initialize($el, opts);
+	};
+
+	PluginCounter.defaults = {
+		accX: 0,
+		accY: 0,
+		speed: 3000,
+		refreshInterval: 100,
+		decimals: 0,
+		onUpdate: null,
+		onComplete: null
+	};
+
+	PluginCounter.prototype = {
+		initialize: function($el, opts) {
+			if ($el.data(instanceName)) {
+				return this;
+			}
+
+			this.$el = $el;
+
+			this
+				.setData()
+				.setOptions(opts)
+				.build();
+
+			return this;
+		},
+
+		setData: function() {
+			this.$el.data(instanceName, this);
+
+			return this;
+		},
+
+		setOptions: function(opts) {
+			this.options = $.extend(true, {}, PluginCounter.defaults, opts, {
+				wrapper: this.$el
+			});
+
+			return this;
+		},
+
+		build: function() {
+			if (!($.isFunction($.fn.countTo))) {
+				return this;
+			}
+
+			var self = this,
+				$el = this.options.wrapper;
+
+			$.extend(self.options, {
+				onComplete: function() {
+					if ($el.data('append')) {
+						$el.html($el.html() + $el.data('append'));
+					}
+
+					if ($el.data('prepend')) {
+						$el.html($el.data('prepend') + $el.html());
+					}
+				}
+			});
+
+			$el.appear(function() {
+
+				$el.countTo(self.options);
+
+			}, {
+				accX: self.options.accX,
+				accY: self.options.accY
+			});
+
+			return this;
+		}
+	};
+
+	// expose to scope
+	$.extend(theme, {
+		PluginCounter: PluginCounter
+	});
+
+	// jquery plugin
+	$.fn.themePluginCounter = function(opts) {
+		return this.map(function() {
+			var $this = $(this);
+
+			if ($this.data(instanceName)) {
+				return $this.data(instanceName);
+			} else {
+				return new PluginCounter($this, opts);
+			}
+
+		});
+	}
+
+}).apply(this, [window.theme, jQuery]);
