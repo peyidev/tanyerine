49dc0c4d826ac0108cb2ae84ed2e5685862127c7
Pedro Laris <peyi.god@gmail.com>
2018-08-22 00:04:28 -0700

    Fix

--- app/code/local/Sual/Importer/controllers/Adminhtml/ImportexecuteController.php
+++ app/code/local/Sual/Importer/controllers/Adminhtml/ImportexecuteController.php
@@ -94,7 +94,7 @@ class Sual_Importer_Adminhtml_ImportexecuteController extends Mage_Adminhtml_Con
                 );
 
                 if( $this->getRequest()->getParam('tipo_ejecucion') == "import_productos"){
-                    shell_exec("/usr/bin/php  " . Mage::getBaseDir('base') . "/shell/import_products.php --source frontend --executionid " . $model->getId() . " > /dev/null &");
+                    echo shell_exec("/usr/bin/php  " . Mage::getBaseDir('base') . "/shell/import_products.php --source frontend --executionid " . $model->getId());
                     //shell_exec("/Applications/MAMP/bin/php/php5.6.32/bin/php  " . Mage::getBaseDir('base') . "/shell/import_products.php --source frontend --executionid " . $model->getId() . " > /dev/null &");
                 }
 
