02bc09d70ade394275fe93b05b95d73309b990cf
Pedro Laris <peyi.god@gmail.com>
2018-09-09 15:44:46 -0700

    Reward poings

---
+++ app/code/community/J2t/Rewardapi/Helper/Data.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * J2T RewardsAPI
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@j2t-design.com so we can send you a copy immediately.
+ *
+ * @category   Magento extension
+ * @package    J2t_Rewardsapi
+ * @copyright  Copyright (c) 2012 J2T DESIGN. (http://www.j2t-design.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class J2t_Rewardapi_Helper_Data extends Mage_Core_Helper_Abstract {
+    
+}
---
+++ app/code/community/J2t/Rewardapi/Model/Api/Resource.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * J2T RewardsAPI
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@j2t-design.com so we can send you a copy immediately.
+ *
+ * @category   Magento extension
+ * @package    J2t_Rewardsapi
+ * @copyright  Copyright (c) 2012 J2T DESIGN. (http://www.j2t-design.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Reward API
+ *
+ * @category   Community
+ * @package    J2t_Rewardapi
+ * @author     J2T Design <contact@j2t-design.net>
+ */
+class J2t_Rewardapi_Model_Api_Resource extends Mage_Api_Model_Resource_Abstract
+{
+
+} // Class J2t_Rewardapi_Model_Api_Resource End
