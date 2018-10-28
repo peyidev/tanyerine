4a5d09c30f48461dd7765bea1d9d010dbea3c3a4
Pedro Laris <pedro.uribe@chromeriver.com>
2018-10-28 15:09:47 -0600

    Reward points

---
+++ app/code/local/Sual/Integrations/Helper/Sualrewards.php
@@ -0,0 +1,211 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: palberto
+ * Date: 10/18/18
+ * Time: 12:17 PM
+ */
+class Sual_Integrations_Helper_Sualrewards extends Mage_Core_Helper_Abstract
+{
+
+    public $customer;
+
+    public function getCustomer($customerId)
+    {
+
+        if (empty($this->customer)) {
+            $customer = Mage::getModel('customer/customer')->load($customerId);
+            $this->customer = $customer;
+        }
+
+        return $this->customer;
+
+    }
+
+    public function balancePoints($customerId)
+    {
+
+        $sbeauty = $this->getTotalRewardsFromOrigin($customerId);
+        $magento = $this->getTotalRewardsFromMagento($customerId);
+
+        if (!$this->rewardsEqual($sbeauty, $magento)) {
+
+
+            $originRewards = $this->getDetailRewardsFromOrigin($customerId);
+            $magentoRewards = $this->getDetailRewardsFromMagento($customerId);
+            $magentoControlKeys = array_column($magentoRewards,'rewardpoints_description');
+            $magentoControlOrders = array_column($magentoRewards,'order_id');
+
+            foreach($originRewards as $oReward){
+
+                if(!in_array($oReward['ctrl_entry'], $magentoControlKeys) && !in_array($oReward['ctrl_entry'], $magentoControlOrders)){
+                    $this->add($customerId,$oReward['points'],1, $oReward['ctrl_entry']);
+                }
+
+            }
+
+
+        }
+    }
+
+
+    public function testRewards()
+    {
+        $this->balancePoints(2);
+    }
+
+    public function getDetailRewardsFromMagento($customerId)
+    {
+        $resource = Mage::getSingleton('core/resource');
+        $readConnection = $resource->getConnection('core_read');
+
+        $tableName = $resource->getTableName('rewardpoints_account');
+
+        $query = "SELECT * FROM $tableName  WHERE customer_id = '{$customerId}'";
+        $magentoRewards = $readConnection->fetchAll($query);
+
+        return $magentoRewards;
+    }
+
+    public function getDetailRewardsFromOrigin($customerId)
+    {
+        $new_db_resource = Mage::getSingleton('core/resource');
+        $connection = $new_db_resource->getConnection('import_db');
+        $customerPhone = $this->getCustomer($customerId)->getData('phone');
+        $detailPoints = $connection->query('SELECT * FROM sb_member_track_points WHERE id_unique_member = "' . $customerPhone . '"');
+        $result = array();
+        foreach ($detailPoints as $detail) {
+            $result[] = $detail;
+        }
+
+        return $result;
+    }
+
+    public function getTotalRewardsFromOrigin($customerId)
+    {
+
+        $new_db_resource = Mage::getSingleton('core/resource');
+        $connection = $new_db_resource->getConnection('import_db');
+        $customerPhone = $this->getCustomer($customerId)->getData('phone');
+        $totalPoints = $connection->query('SELECT * FROM sb_member_points WHERE id_unique_member = "' . $customerPhone . '"');
+        $total = 0;
+        foreach ($totalPoints as $point) {
+            $total = $point['points'];
+            break;
+        }
+
+        return intval($total);
+    }
+
+
+    public function getTotalRewardsFromMagento($customerId)
+    {
+        return intval($this->info($customerId, 1)['current']);
+    }
+
+    public function rewardsEqual($p1, $p2)
+    {
+        return (abs($p1 - $p2) <= 1) ? true : false;
+    }
+
+
+    /**
+     * Retrieve customer points data
+     *
+     * @param int $customerId
+     * @param int $storeId
+     * @return array
+     */
+    public function info($customerId, $storeId)
+    {
+        $result = array();
+
+        $customer = $this->getCustomer($customerId);
+        if (!$customer->getId()) {
+            $this->_fault('data_invalid');
+        }
+
+        if (trim($storeId) == "") {
+            $this->_fault('data_invalid');
+        }
+
+
+        if (Mage::getStoreConfig('rewardpoints/default/flatstats', $storeId)) {
+            $reward_flat_model = Mage::getModel('rewardpoints/flatstats');
+            $result['current'] = $reward_flat_model->collectPointsCurrent($customerId, $storeId);
+            $result['received'] = $reward_flat_model->collectPointsReceived($customerId, $storeId);
+            $result['spent'] = $reward_flat_model->collectPointsSpent($customerId, $storeId);
+            $result['waiting'] = $reward_flat_model->collectPointsWaitingValidation($customerId, $storeId);
+            $result['lost'] = $reward_flat_model->collectPointsLost($customerId, $storeId);
+        } else {
+            $reward_model = Mage::getModel('rewardpoints/stats');
+            $result['current'] = $reward_model->getPointsCurrent($customerId, $storeId);
+            $result['received'] = $reward_model->getRealPointsReceivedNoExpary($customerId, $storeId);
+            $result['spent'] = $reward_model->getPointsSpent($customerId, $storeId);
+            $result['waiting'] = $reward_model->getPointsWaitingValidation($customerId, $storeId);
+            $result['lost'] = $reward_model->getRealPointsLost($customerId, $storeId);
+        }
+
+        return $result;
+    }
+
+
+
+    /**
+     * Add points data
+     *
+     * @param int $customerId, $points
+     * @param string $storeIds
+     * @param string $originKey
+     * @return boolean
+     */
+    public function add($customerId, $points, $storeIds, $originKey)
+    {
+        $customer = $this->getCustomer($customerId);
+
+        if (!$customer->getId()) {
+            $this->_fault('not_exists');
+        }
+
+        if (!$points) {
+            $this->_fault('not_exists');
+        }
+
+        $store_array = explode(",", $storeIds);
+        if ($store_array != array()){
+            foreach ($store_array as $storeId){
+                $current_store = Mage::getModel('core/store')->load(trim($storeId));
+                if (!$current_store->getId()){
+                    $this->_fault('not_exists');
+                }
+            }
+        } else {
+            $this->_fault('not_exists');
+        }
+
+        $data = array(
+            (($points > 0) ? 'points_current' :  'points_spent') => $points,
+            'customer_id' => $customerId,
+            'store_id' => $storeIds,
+            'order_id' => J2t_Rewardpoints_Model_Stats::TYPE_POINTS_ADMIN,
+            'date_start' => date("Y-m-d"),
+            'rewardpoints_description' => $originKey);
+
+        $model = Mage::getModel('rewardpoints/stats');
+        $model->setData($data);
+
+        $model->save();
+        if ($store_array != array()){
+            foreach ($store_array as $storeId){
+                Mage::getModel('rewardpoints/flatstats')->processRecordFlat($customerId, $storeId);
+            }
+        }
+
+
+        return true;
+    }
+
+
+
+}
--- shell/testServices.php
+++ shell/testServices.php
@@ -17,7 +17,9 @@ class Mage_Shell_Test_Services extends Mage_Shell_Abstract
 
     public function run()
     {
-        $helper =  Mage::helper('sual_integrations/data');
+
+        $helper = Mage::helper('sual_integrations/sualrewards');
+//        $helper =  Mage::helper('sual_integrations/data');
 //        $response = $helper->callService("mensajeria/cotizar",'{"carrier":"dhlexpress","calle":"Cerrada de la Talavera","colonia":"El Cobano","codigoPostal":"53040","municipio":"San antonio de ayala","ciudad":"Irapuato","estado":"guanajuato","pais":"Mexico"}');
 //        $response = $helper->callService("mensajeria/cotizar",'{"carrier":"estafeta","calle":"Cerrada de la Talavera","colonia":"El Cobano","codigoPostal":"53040","municipio":"San antonio de ayala","ciudad":"Irapuato","estado":"guanajuato","pais":"Mexico"}');
 
@@ -25,35 +27,36 @@ class Mage_Shell_Test_Services extends Mage_Shell_Abstract
 
         //QPARAM EXAMPLE
         //BillTo_firstName=Pedro&BillTo_lastName=Laris&BillTo_street=Cerrada+de+la+Talavera&BillTo_streetNumber=19&BillTo_streetNumber2=&BillTo_street2Col=El+Cobano&BillTo_street2Del=San+antonio+de+ayala&BillTo_city=Irapuato&BillTo_state=guanajuato&BillTo_country=MX&BillTo_phoneNumber=5537228701&BillTo_postalCode=36600&BillTo_email=peyi.god%40gmail.com&Card_accountNumber=5454545454545454&Card_cardType=002&Card_expirationMonth=12&Card_expirationYear=2020&PurchaseTotals_grandTotalAmount=10&DeviceFingerprintID=10012040&Card_cardCCV=329&Card_cardPromotion=0
+//
+//        $params = array();
+//        $params['BillTo_firstName'] = "Pedro";
+//        $params['BillTo_lastName'] = "Laris";
+//        $params['BillTo_street'] = "Cerrada+de+la+Talavera";
+//        $params['BillTo_streetNumber'] = "19a";
+//        $params['BillTo_streetNumber2'] = " ";
+//        $params['BillTo_street2Col'] = "Colonia";
+//        $params['BillTo_street2Del'] = "Delegacion+No+Se";
+//        $params['BillTo_city'] = "Ciudad";
+//        $params['BillTo_state'] = "Estado";
+//        $params['BillTo_country'] = "MX";
+//        $params['BillTo_phoneNumber'] = "1231231231";
+//        $params['BillTo_postalCode'] = "53040";
+//        $params['BillTo_email'] = "CORREO@CORREO.COM";
+//        $params['Card_accountNumber'] = "5454545454545454";
+//        //002 PARA MASTERCARD - 001 PARA VISA
+//        $params['Card_cardType'] = "002";
+//        $params['Card_expirationMonth'] = "12";
+//        $params['Card_expirationYear'] = "2020";
+//        $params['Card_cardCCV'] = "123";
+//        $params['PurchaseTotals_grandTotalAmount'] = "100";
+//        //0 3 6
+//        $params['Card_cardPromotion'] = "0";
+//        $params['DeviceFingerprintID'] = rand(10000000, 19999999);
+//
+//        $response = $helper->callService("procesa/banorte",$params);
+//        print_r($response);
 
-        $params = array();
-        $params['BillTo_firstName'] = "Pedro";
-        $params['BillTo_lastName'] = "Laris";
-        $params['BillTo_street'] = "Cerrada+de+la+Talavera";
-        $params['BillTo_streetNumber'] = "19a";
-        $params['BillTo_streetNumber2'] = " ";
-        $params['BillTo_street2Col'] = "Colonia";
-        $params['BillTo_street2Del'] = "Delegacion+No+Se";
-        $params['BillTo_city'] = "Ciudad";
-        $params['BillTo_state'] = "Estado";
-        $params['BillTo_country'] = "MX";
-        $params['BillTo_phoneNumber'] = "1231231231";
-        $params['BillTo_postalCode'] = "53040";
-        $params['BillTo_email'] = "CORREO@CORREO.COM";
-        $params['Card_accountNumber'] = "5454545454545454";
-        //002 PARA MASTERCARD - 001 PARA VISA
-        $params['Card_cardType'] = "002";
-        $params['Card_expirationMonth'] = "12";
-        $params['Card_expirationYear'] = "2020";
-        $params['Card_cardCCV'] = "123";
-        $params['PurchaseTotals_grandTotalAmount'] = "100";
-        //0 3 6
-        $params['Card_cardPromotion'] = "0";
-        $params['DeviceFingerprintID'] = rand(10000000, 19999999);
-
-        $response = $helper->callService("procesa/banorte",$params);
-        print_r($response);
-
+        $helper->testRewards();
 
     }
 
