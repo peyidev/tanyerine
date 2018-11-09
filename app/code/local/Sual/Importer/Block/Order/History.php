a8c40b04d081ce4d897dd26b9795e9a804648eec
Pedro Laris <pedro.uribe@chromeriver.com>
2018-11-09 14:03:16 -0600

    Inventario

--- app/code/local/Sual/Importer/Block/Order/History.php
+++ app/code/local/Sual/Importer/Block/Order/History.php
@@ -72,7 +72,7 @@ class Sual_Importer_Block_Order_History  extends Mage_Sales_Block_Order_History
         $query = "SELECT AES_DECRYPT(sb.password,'SualBeauty') as decryptedpassword, sb.* from sb_member AS sb WHERE email = \"{$email}\";";
         $customerData = $connection->raw_fetchRow($query);
 
-        $query = "SELECT * FROM `sb_shoppingcart` WHERE `id_member` = ".$customerData['id_member']." and `subtotal` IS NOT NULL;";
+        $query = "SELECT * FROM `sb_shoppingcart` WHERE `id_member` = ".$customerData['id_member']." AND `subtotal` IS NOT NULL AND payment_status <> 'MAGENTO';";
         $customerOrders = $connection->query($query);
         return $customerOrders;
     }
