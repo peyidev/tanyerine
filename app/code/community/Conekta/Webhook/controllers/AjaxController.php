fc47bc03748a02d20742acf6e9890cff0785ba66
Eduardo Garcia <you@example.com>
2018-11-10 14:34:59 -0800

    Remove validations

--- app/code/community/Conekta/Webhook/controllers/AjaxController.php
+++ app/code/community/Conekta/Webhook/controllers/AjaxController.php
@@ -56,9 +56,6 @@ class Conekta_Webhook_AjaxController extends Mage_Core_Controller_Front_Action {
   }
 
   public function authenticateEvent($body, $digest) {
-    if ($digest != "Conekta Webhook Notifier") {
-      throw new Exception("Empty digest");
-    }
     /*$private_key_string = Mage::getStoreConfig('payment/webhook/signature_key');
     if (!empty($private_key_string) && !empty($body)) {
       if (!empty($digest)) {
