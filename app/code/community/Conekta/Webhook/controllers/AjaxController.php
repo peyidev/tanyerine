47722d5ab8ee89baf431efd2fa7d09247e162820
Pedro Laris <peyi.god@gmail.com>
2018-11-10 13:37:33 -0800

    Change validaiton

--- app/code/community/Conekta/Webhook/controllers/AjaxController.php
+++ app/code/community/Conekta/Webhook/controllers/AjaxController.php
@@ -1,52 +1,55 @@
 <?php
 class Conekta_Webhook_AjaxController extends Mage_Core_Controller_Front_Action {
   public function listenerAction() {
-    $body = @file_get_contents('php://input');
-    Mage::log($body);
-    Mage::log(json_encode($_SERVER));
-    self::authenticateEvent($body, $_SERVER['HTTP_USER_AGENT']);
-    $event = json_decode($body);
-    $charge = $event->data->object;
-    sleep(2);
-    $charge_id = $charge->metadata->checkout_id;
-    $charge_id_matches_format = preg_match('/^[a-z_\-0-9]+$/i', $charge_id);
-    if ($charge_id_matches_format) {
-        $resource = Mage::getSingleton('core/resource');
-        $readConnection = $resource->getConnection('core_read');
-        $query = "SELECT parent_id FROM " . $resource->getTableName('sales/order_payment') . " WHERE charge_id = '" . $charge_id . "' LIMIT 1";
-        $entity_id = $readConnection->fetchOne($query);
-    }
+    try {
+      $body = @file_get_contents('php://input');
+      Mage::log($body);
+      Mage::log(json_encode($_SERVER));
+      self::authenticateEvent($body, $_SERVER['HTTP_USER_AGENT']);
+      $event = json_decode($body);
+      $charge = $event->data->object;
+      sleep(2);
+      $charge_id = $charge->metadata->checkout_id;
+      $charge_id_matches_format = preg_match('/^[a-z_\-0-9]+$/i', $charge_id);
+      if ($charge_id_matches_format) {
+          $resource = Mage::getSingleton('core/resource');
+          $readConnection = $resource->getConnection('core_read');
+          $query = "SELECT parent_id FROM " . $resource->getTableName('sales/order_payment') . " WHERE charge_id = '" . $charge_id . "' LIMIT 1";
+          $entity_id = $readConnection->fetchOne($query);
+      }
 
-    $entity_id_matches_format = preg_match('/^[0-9]+$/i', $entity_id);
-    if ($entity_id_matches_format) {
-      $query = 'SELECT increment_id FROM ' . $resource->getTableName('sales/order') . " WHERE entity_id = '" . $entity_id . "' LIMIT 1";
-      $increment_id = $readConnection->fetchOne($query);
-      $order = Mage::getModel('sales/order')->loadByIncrementId($increment_id);
-    }
-    if ($charge_id_matches_format && $entity_id_matches_format && $event->type == "order.paid" && $order->getId()) {   
-      if ($order->hasInvoices() != true) {
-        $invoice = $order->prepareInvoice();
-        $invoice->register();
-        Mage::getModel('core/resource_transaction')
-        ->addObject($invoice)
-        ->addObject($invoice->getOrder())
-        ->save();
-        $invoice->sendEmail(true, '');
+      $entity_id_matches_format = preg_match('/^[0-9]+$/i', $entity_id);
+      if ($entity_id_matches_format) {
+        $query = 'SELECT increment_id FROM ' . $resource->getTableName('sales/order') . " WHERE entity_id = '" . $entity_id . "' LIMIT 1";
+        $increment_id = $readConnection->fetchOne($query);
+        $order = Mage::getModel('sales/order')->loadByIncrementId($increment_id);
       }
-      $orderStatus = $order->getPayment()->getMethodInstance()->getConfigData('webhook_notification_order_status');
-      if (!(strpos($order->getStatus(), $orderStatus) !== false)) {
-        $order->addStatusToHistory($orderStatus);
-        $order->setData('state', $orderStatus);
-        $order->save();
+      if ($charge_id_matches_format && $entity_id_matches_format && $event->type == "order.paid" && $order->getId()) {   
+        if ($order->hasInvoices() != true) {
+          $invoice = $order->prepareInvoice();
+          $invoice->register();
+          Mage::getModel('core/resource_transaction')
+          ->addObject($invoice)
+          ->addObject($invoice->getOrder())
+          ->save();
+          $invoice->sendEmail(true, '');
+        }
+        $orderStatus = $order->getPayment()->getMethodInstance()->getConfigData('webhook_notification_order_status');
+        if (!(strpos($order->getStatus(), $orderStatus) !== false)) {
+          $order->addStatusToHistory($orderStatus);
+          $order->setData('state', $orderStatus);
+          $order->save();
+        }
+      } else {
+        if ($event->type == "order.paid"){
+          header('HTTP/1.0 404 Not Found');
+          $this->getResponse()->setHeader('HTTP/1.1','404 Not Found');
+          $this->getResponse()->setHeader('Status','404 File not found');
+          $this->_forward('defaultNoRoute');
+        }
       }
-    } else {
-      if ($event->type == "order.paid"){
-        header('HTTP/1.0 404 Not Found');
-        $this->getResponse()->setHeader('HTTP/1.1','404 Not Found');
-        $this->getResponse()->setHeader('Status','404 File not found');
-        $this->_forward('defaultNoRoute');
-      }
-    }
+      
+    } catch (Exception $e) {}
   }
 
   public function authenticateEvent($body, $digest) {
