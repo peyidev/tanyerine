d154b3fd07090c19f93b79900954c1c6b2c1f5cf
Pedro Laris <pedro.uribe@chromeriver.com>
2018-11-10 17:57:38 -0600

    Fix balanceo

--- app/code/local/Sual/Integrations/Helper/Sualrewards.php
+++ app/code/local/Sual/Integrations/Helper/Sualrewards.php
@@ -56,7 +56,9 @@ class Sual_Integrations_Helper_Sualrewards extends Mage_Core_Helper_Abstract
             if(!empty($hasPoints))
                 return false;
 
-            $this->addRemote($customerId, $orderId, $pointsToExport[0]);
+            foreach($pointsToExport as $points){
+                $this->addRemote($customerId, $orderId, $points);
+            }
 
         }
 
