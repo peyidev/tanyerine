5a923f08d436a593ffde63002d63d1e57fdbf5fb
Pedro Laris <peyi.god@gmail.com>
2018-11-19 09:54:46 -0800

    cache

--- app/code/local/Sual/Forms/Block/Categories.php
+++ app/code/local/Sual/Forms/Block/Categories.php
@@ -1,5 +1,17 @@
 <?php
 class Sual_Forms_Block_Categories extends Mage_Core_Block_Abstract implements Mage_Widget_Block_Interface {
+
+
+    protected function _construct()
+    {
+        $this->addData(array(
+            'cache_lifetime' => 3600,
+            'cache_tags'     => array(Mage_Catalog_Model_Product::CACHE_TAG),
+            'cache_key'      => Mage::registry('current_category') . "-widget-cache",
+        ));
+    }
+
+
 /**
   * Produce links list rendered as html
   *
