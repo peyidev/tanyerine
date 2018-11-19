8cef6f211d591a85d5e4ed74b8ad1c6fadc77f05
Pedro Laris <peyi.god@gmail.com>
2018-11-19 12:39:10 -0800

    Fix js

--- skin/frontend/smartwave/porto/js/sual.js
+++ skin/frontend/smartwave/porto/js/sual.js
@@ -9,4 +9,20 @@ jQuery(document).ready(function(){
             scrollTop: jQuery(id).offset().top - 100
         }, 500);
     });
+    jQuery('body').on('click','.layer-filter-icon, .close-mobile-layer, .close-layer',function(event) { 
+        if(!jQuery('body').hasClass('mobile-layer-shown')) {
+            jQuery('body').addClass('mobile-layer-shown', function() { 
+                setTimeout(function(){
+                    jQuery(document).one("click",function(e) {
+                        var target = e.target;
+                        if (!jQuery(target).is('.block-main-layer .block') && !jQuery(target).parents().is('.block-main-layer .block')) {
+                                    jQuery('body').removeClass('mobile-layer-shown');
+                        }
+                    });  
+                }, 111);
+            });
+        } else{
+            jQuery('body').removeClass('mobile-layer-shown');
+        }
+    }); 
 });
