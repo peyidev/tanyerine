970f3aeb4932cfcc3df30b3098ad6e78b3f3fdf2
Pedro Laris <peyi.god@gmail.com>
2018-11-19 10:03:07 -0800

    remove tmp cache

--- app/code/local/Sual/Forms/Block/Categories.php
+++ app/code/local/Sual/Forms/Block/Categories.php
@@ -5,7 +5,7 @@ class Sual_Forms_Block_Categories extends Mage_Core_Block_Abstract implements Ma
     protected function _construct()
     {
         $this->addData(array(
-            'cache_lifetime' => 3600,
+            'cache_lifetime' => 0,
             'cache_key'      => Mage::registry('current_category')->getId() . "-widget-cache",
         ));
 
