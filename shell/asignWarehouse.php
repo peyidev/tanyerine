347b660b3ec09788f05d78f4f24ac3ebd9b465f9
Pedro Laris <peyi.god@gmail.com>
2018-11-20 14:09:32 -0800

    fix

--- shell/asignWarehouse.php
+++ shell/asignWarehouse.php
@@ -42,11 +42,12 @@ class Mage_Shell_AsignWarehouse extends Mage_Shell_Abstract
     {
 
         $query = "SELECT id_member FROM sb_member WHERE email = '{$email}'";
-        echo $query . "->";
         $new_db_resource = Mage::getSingleton('core/resource');
         $connection = $new_db_resource->getConnection('import_db');
 
         $customerData = $connection->raw_fetchRow($query);
+
+        print_r($customerData);
 
         if (!empty($customerData['id_member']))
             return $customerData['id_member'];
