e6463f83cc3e3b553fdfd9a4cfa5c670dbac3942
Pedro Laris <peyi.god@gmail.com>
2018-11-22 11:06:51 -0800

    fix

--- app/code/local/Sual/Integrations/Model/Observer.php
+++ app/code/local/Sual/Integrations/Model/Observer.php
@@ -14,7 +14,7 @@ class Sual_Integrations_Model_Observer extends Varien_Event_Observer
 
             Mage::log("Producto" . $product->getSku() . " StockReal -> " . $stock . " ActualMagento -> " . $stockItem->getQty(), null, 'stock.log');
             if ($stock != 'error' && $stockItem->getQty() != $stock) {
-                Mage::log("cambiando stock");
+                Mage::log("cambiando stock",null, 'stock.log');
                 $stockItem->setData('is_in_stock', ($stock > 0) ? 1 : 0);
                 $stockItem->setData('stock_id', 1);
                 $stockItem->setData('store_id', 1);
